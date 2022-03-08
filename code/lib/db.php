91bc66baa7d20d49774c3bce39aef60f1bff66f2
Connor Laidlaw <ubuntu@ubuntu2004.linuxvmimages.local>
2022-03-07 20:36:08 -0500

    Add prepared statements to prevent against SQLi

--- code/lib/db.php
+++ code/lib/db.php
@@ -1,6 +1,7 @@
 <?php
 
 $dbconn = pg_pconnect("host=$pg_host port=$pg_port dbname=$pg_dbname user=$pg_dbuser password=$pg_dbpassword") or die("Could not connect");
+
 if ($debug) {
 	echo "host=$pg_host, port=$pg_port, dbname=$pg_dbname, user=$pg_dbuser, password=$pg_dbpassword<br>";
 	$stat = pg_connection_status($dbconn);
@@ -11,11 +12,21 @@ if ($debug) {
 	}    
 }
 
-function run_query($dbconn, $query) {
+function run_query($dbconn, $query, $inArray) {
 	if ($debug) {
 		echo "$query<br>";
 	}
-	$result = pg_query($dbconn, $query);
+	if($inArray!=NULL){
+		$result = pg_query_params($dbconn, $query, $inArray);
+		#run_query is now edited to take a prepared statement as an input variable, with an array of inputs
+		#This statement will be executed if the given task takes any user input
+	}
+	else{
+		$result = pg_query($dbconn, $query);
+		#This will be executed if no userinput is taken, such as the get_article function
+	}
+	
+
 	if ($result == False and $debug) {
 		echo "Query failed<br>";
 	}
@@ -37,31 +48,32 @@ function get_article_list($dbconn){
 		authors ON articles.author=authors.id
 		ORDER BY
 		date DESC";
-return run_query($dbconn, $query);
+return run_query($dbconn, $query,NULL);
 }
 
 function get_article($dbconn, $aid) {
-	$query= 
-		"SELECT 
-		articles.created_on as date,
-		articles.aid as aid,
-		articles.title as title,
-		authors.username as author,
-		articles.stub as stub,
-		articles.content as content
-		FROM 
-		articles
-		INNER JOIN
-		authors ON articles.author=authors.id
-		WHERE
-		aid='".$aid."'
-		LIMIT 1";
-return run_query($dbconn, $query);
+	$query = 'SELECT 
+	articles.created_on as date,
+	articles.aid as aid,
+	articles.title as title,
+	authors.username as author,
+	articles.stub as stub,
+	articles.content as content
+	FROM 
+	articles
+	INNER JOIN
+	authors ON articles.author=authors.id
+	WHERE
+	aid=$1
+	LIMIT 1';
+	return run_query($dbconn,$query,array($aid));
+
 }
 
 function delete_article($dbconn, $aid) {
-	$query= "DELETE FROM articles WHERE aid='".$aid."'";
-	return run_query($dbconn, $query);
+	$query= "DELETE FROM articles WHERE aid=$1";
+
+	return run_query($dbconn, $query,array($aid));
 }
 
 function add_article($dbconn, $title, $content, $author) {
@@ -72,19 +84,19 @@ function add_article($dbconn, $title, $content, $author) {
 		articles
 		(aid, title, author, stub, content) 
 		VALUES
-		('$aid', '$title', $author, '$stub', '$content')";
-	return run_query($dbconn, $query);
+		($1, $2, $3, $4, $5)";
+	return run_query($dbconn, $query,array($aid,$title,$author,$stub,$content));
 }
 
 function update_article($dbconn, $title, $content, $aid) {
 	$query=
 		"UPDATE articles
 		SET 
-		title='$title',
-		content='$content'
+		title=$1,
+		content=$2
 		WHERE
-		aid='$aid'";
-	return run_query($dbconn, $query);
+		aid=$3";
+	return run_query($dbconn, $query,array($title,$content,$aid));
 }
 
 function authenticate_user($dbconn, $username, $password) {
@@ -97,10 +109,11 @@ function authenticate_user($dbconn, $username, $password) {
 		FROM
 		authors
 		WHERE
-		username='".$_POST['username']."'
+		username=$1
 		AND
-		password='".$_POST['password']."'
+		password=$2
 		LIMIT 1";
-	return run_query($dbconn, $query);
+
+	return run_query($dbconn, $query,array($_POST['username'],$_POST['password']));
 }	
 ?>
